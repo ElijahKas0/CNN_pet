c855c5f3b9f8bee4c16009e6d41c07346c2aed81
ElijahWood228 <maestrokdm2000@gmail.com>
2025-06-16 23:53:57 +0300

    Conclusion/Fin

--- NN_particle.py
+++ NN_particle.py
@@ -1,4 +1,7 @@
 import numpy as np
+
+np.random.seed(1)
+
 streetlights = np.array( [[ 1, 0, 1 ],
     [ 0, 1, 1],
     [ 0, 0, 1],
@@ -12,37 +15,35 @@ def relu2deriv(x):
     return x>0
 
 layer_1_values = 4
+hidden_size = 4
+weights_0_1 = 2*np.random.random((3,hidden_size)) - 1
+weights_1_2 = 2*np.random.random((hidden_size,1)) - 1
+print(weights_0_1)
+print(weights_1_2)
 
-weight_layer_0_1 = []
-for i in range(layer_1_values):
-    weight_layer_0_1.append(np.random.rand(3))
-weight_layer_0_1 = np.array(weight_layer_0_1)
-weight_layer_1_2 = np.random.rand(layer_1_values)
-
-# print(weight_layer_0_1)
-# print(weight_layer_1_2)
-alpha = 0.01
+alpha = 0.2
 
 for i in range(60):
     error_sum = 0
     for i in range(len(streetlights)):
         goal = walk_vs_stop[i:i+1]
+        # print(goal,type(goal))
         layer_0 = streetlights[i:i+1]
-        print(layer_0,goal,weight_layer_0_1)
-        layer_1 = np.array(relu(layer_0.dot(weight_layer_0_1.T)))
-        print(layer_1)
-        layer_2 = np.array(layer_1.dot(weight_layer_1_2.T))
-        print(layer_2)
+        # print(layer_0,type(layer_0))
+        layer_1 = relu(layer_0.dot(weights_0_1.T))
+        # print(layer_1,type(layer_1))
+        layer_2 = layer_1.dot(weights_1_2.T)
+        # print(layer_2,type(layer_1))
         error = (layer_2-goal)**2
-        error_sum += error
+        error_sum += np.sum(error)
         delta_2 = np.array(layer_2 - goal) 
-        delta_1 = np.array(delta_2)*weight_layer_1_2
-        print(delta_2,layer_1)
-        weight_layer_1_2 -= alpha*(delta_2[0]*layer_1[0])
-        print(delta_1[0],layer_0[0])
-        weight_layer_0_1 -= alpha*(np.array(delta_1[0])*np.array(layer_0[0])*relu2deriv(layer_1))
-    if i % 10 == 9:
-        print(error,error_sum)
+        delta_1 = np.array(delta_2)*weights_1_2
+        # print(weight_layer_1_2)
+        weights_1_2 -= alpha*delta_2.dot(layer_1)
+        # print(alpha*delta_1.T.dot(layer_0))
+        weights_0_1 -= alpha*delta_1.T.dot(layer_0)*relu2deriv(layer_1.T)
+    
+    print(error_sum)
 
 
 
