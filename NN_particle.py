695d0e9c7a6ae8b96fed9f03002a86166cd8e25a
Касьянов Илья Дмитриевич <ilya.kasyanov@kate.local>
2025-06-17 18:35:22 +0300

    Важное по нейросетям

--- NN_particle.py
+++ NN_particle.py
@@ -1,11 +1,83 @@
+# import numpy as np
+
+# # weights_0_1 = np.array([[-0.3,-0.22,-0.29,-0.07]])
+# # print(weights_0_1)
+# # input = np.array([[1,0,1]])
+# # print(weights_0_1.T.dot(input))
+# # print(weights_0_1)
+
+# np.random.seed(42)
+
+
+# streetlights = np.array( [[ 1, 0, 1],
+#     [ 0, 1, 1 ],
+#     [ 0, 0, 1],
+#     [1,1,1]])
+# walk_vs_stop = np.array([[ 1, 1, 0, 0]]).T
+
+# def relu(x):
+#     return (x>0)*x
+
+# def relu2deriv(x):
+#     return x>0
+
+# weights_0_1 = np.array([[-0.3, 0.5, -0.8],[-0.4,0.2,0.6],[0.1,-0.5,0.7],[0.6,-0.3,0.9]])
+# weights_1_2 = np.array([[0.5],[-0.3],[0.4],[0.1]])
+# print(weights_0_1,weights_1_2)
+# weights_0_1 = 2*np.random.rand(4,3)-1
+# weights_1_2 = 2*np.random.rand(4,1)-1
+# print(weights_0_1,weights_1_2)
+# alpha = 0.05
+
+# for iteration in range(1000):
+#     layer_2_error = 0
+#     for iter in range(len(streetlights)):
+#         layer_0 = streetlights[iter:iter+1]
+        
+#         goal = walk_vs_stop[iter:iter+1]
+        
+#         layer_1 = relu(layer_0.dot(weights_0_1.T))
+        
+#         layer_2 = layer_1.dot(weights_1_2)
+        
+#         error = (layer_2-goal)**2
+        
+#         layer_2_error +=np.sum(error)
+#         delta_2 = layer_2-goal
+        
+        
+        
+#         delta_1 = delta_2.dot(weights_1_2.T)*relu2deriv(layer_1)
+#         # print(delta_1)
+#         weighted_delta_2 = alpha*(layer_1.T.dot(delta_2))
+        
+#         weighted_delta_1 = alpha*(delta_1.T.dot(layer_0))
+#         # print(weighted_delta_1)
+        
+        
+        
+#         weights_1_2 -= weighted_delta_2
+
+#         weights_0_1 -= weighted_delta_1
+#         # print(error,layer_1,layer_2,layer_0)
+        
+#     if iteration % 10 == 9:
+#         print(f"Error{layer_2_error}")
+
+
 import numpy as np
 
-np.random.seed(1)
+# weights_0_1 = np.random.random((4,3))
+# weights_1_2 = np.random.random((4,1))
+# print(weights_0_1)
+# print(weights_1_2)
+
+np.random.seed(42)
 
 streetlights = np.array( [[ 1, 0, 1 ],
-    [ 0, 1, 1],
-    [ 0, 0, 1],
-    [1,1,1]])
+[ 0, 1, 1],
+[ 0, 0, 1],
+[1,1,1]])
 walk_vs_stop = np.array([[ 1, 1, 0, 0]]).T
 
 def relu(x):
@@ -14,36 +86,39 @@ def relu(x):
 def relu2deriv(x):
     return x>0
 
-layer_1_values = 4
-hidden_size = 4
-weights_0_1 = 2*np.random.random((3,hidden_size)) - 1
-weights_1_2 = 2*np.random.random((hidden_size,1)) - 1
-print(weights_0_1)
-print(weights_1_2)
+weights_0_1 = 2*np.random.rand(4,3)-1
+weights_1_2 = 2*np.random.rand(4,1)-1
 
 alpha = 0.2
 
-for i in range(60):
-    error_sum = 0
-    for i in range(len(streetlights)):
-        goal = walk_vs_stop[i:i+1]
-        # print(goal,type(goal))
-        layer_0 = streetlights[i:i+1]
-        # print(layer_0,type(layer_0))
+for iteration in range(60):
+    layer_2_error = 0
+    for iter in range(len(streetlights)):
+        layer_0 = streetlights[iter:iter+1]
+        
+        goal = walk_vs_stop[iter:iter+1]
+        
         layer_1 = relu(layer_0.dot(weights_0_1.T))
-        # print(layer_1,type(layer_1))
-        layer_2 = layer_1.dot(weights_1_2.T)
-        # print(layer_2,type(layer_1))
+        
+        layer_2 = layer_1.dot(weights_1_2)
+        
         error = (layer_2-goal)**2
-        error_sum += np.sum(error)
-        delta_2 = np.array(layer_2 - goal) 
-        delta_1 = np.array(delta_2)*weights_1_2
-        # print(weight_layer_1_2)
-        weights_1_2 -= alpha*delta_2.dot(layer_1)
-        # print(alpha*delta_1.T.dot(layer_0))
-        weights_0_1 -= alpha*delta_1.T.dot(layer_0)*relu2deriv(layer_1.T)
-    
-    print(error_sum)
-
+        # print(error)
+        layer_2_error +=np.sum(error)
+        delta_2 = layer_2-goal
+        
+        
+        # print(delta_2)
+        delta_1 = delta_2.dot(weights_1_2.T)*relu2deriv(layer_1)
+        
+        weighted_delta_2 = alpha*(layer_1.T.dot(delta_2))
+        
+        weighted_delta_1 = alpha*(delta_1.T.dot(layer_0))
+        
+        # print(weighted_delta_1)
+        weights_1_2 -= weighted_delta_2
+        weights_0_1 -= weighted_delta_1
+    if iteration % 10 == 9:
+        print(f"Error{layer_2_error}")
 
 
